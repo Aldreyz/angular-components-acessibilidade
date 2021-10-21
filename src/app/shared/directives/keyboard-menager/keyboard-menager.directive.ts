469176c90db08c47706e2c0107ff4e46eeb79729
Aldrey <aldreybp@gmail.com>
2021-10-21 19:44:54 -0300

    Keyboard ManagedItem Directive

--- src/app/shared/directives/keyboard-menager/keyboard-menager.directive.ts
+++ src/app/shared/directives/keyboard-menager/keyboard-menager.directive.ts
@@ -1,13 +1,24 @@
-import { Directive, HostListener } from '@angular/core';
+import { KeyboardMenagedItemDirective } from './keyboard-menaged-item.directive';
+import {
+  ContentChildren,
+  Directive,
+  HostListener,
+  QueryList,
+} from '@angular/core';
 
 @Directive({
   selector: '[appKm]',
 })
 export class KeyboardMenagerDirective {
+
+  @ContentChildren(KeyboardMenagedItemDirective)
+  public items: QueryList<KeyboardMenagedItemDirective> = null;
+
   @HostListener('keyup', ['$event'])
   public menageKeys(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowUp':
+        console.log(this.items);
         console.log('up');
         break;
       case 'ArrowDown':
--- src/app/shared/directives/keyboard-menager/keyboard-menager.module.ts
+++ src/app/shared/directives/keyboard-menager/keyboard-menager.module.ts
@@ -1,11 +1,11 @@
+import { KeyboardMenagedItemDirective } from './keyboard-menaged-item.directive';
 import { KeyboardMenagerDirective } from './keyboard-menager.directive';
 import { CommonModule } from '@angular/common';
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations:[KeyboardMenagerDirective],
-  imports:[CommonModule],
-  exports:[ KeyboardMenagerDirective],
-
+  declarations: [KeyboardMenagerDirective, KeyboardMenagedItemDirective],
+  imports: [CommonModule],
+  exports: [KeyboardMenagerDirective , KeyboardMenagedItemDirective],
 })
-export class KeyboardMenagerModule{}
+export class KeyboardMenagerModule {}
