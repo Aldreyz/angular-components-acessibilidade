821d4d196a92de443c34560bcd04c5214107181f
Aldrey <aldreybp@gmail.com>
2021-10-21 14:01:21 -0300

    Event Binding

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
-  styleUrls: ['./yes-no-button-group.component.scss']
+  styleUrls: ['./yes-no-button-group.component.scss'],
 })
 export class YesNoButtonGroupComponent implements OnInit {
-  @Input()public value:string=null;
-  @Input()public label ='';
+  @Input() public value: string = null;
+  @Input() public label = '';
+  @Output() public valueChange = new EventEmitter<string>();
   public options = yesNoButtonGroupOptions;
-  constructor() { }
 
-  ngOnInit(): void {
-  }
-  public activate(value:string){
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public activate(value: string) {
     this.value = value;
+    this.valueChange.emit(this.value);
   }
 }
-enum yesNoButtonGroupOptions{
-   YES = 'yes',
-   NO ='no'
+enum yesNoButtonGroupOptions {
+  YES = 'yes',
+  NO = 'no',
 }
